23fd4b7654cf3a732a3d2a6ff559493a9570d6c4
Wen Zhou <ericchou19831101@msn.com>
2021-01-10 13:34:56 +0100

    Add example file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # terraformhttp
 a simple example of terraform http provider
+
+# how to
+set proper value for variable "mycity" to search to today's min/max temperature
+
+# why
+just a simple way to see what http provider can be done
+since we have some super way to use python query json from terraform configuration...
---
+++ main.tf
@@ -0,0 +1,59 @@
+#########################################################
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = ">=3.0.0"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = ">= 2.0.0"
+    }
+  }
+  required_version = ">= 0.13.4"
+}
+
+########################################################
+variable "mycity" {
+    type = string
+    default = "gothenburg"
+    description = "where you live"
+}
+
+#########################################################
+data "http" "location" {
+  # url = "https://www.metaweather.com/api/location/search/?lattlong=57.701328,11.96689"
+    url = "https://www.metaweather.com/api/location/search/?query=${var.mycity}"
+    request_headers = {
+        "Accept" = "application/json"
+    }
+}
+
+data "null_data_source" "locationwoeid" {
+    inputs = {
+  	url = "https://www.metaweather.com/api/location/${jsondecode(data.http.location.body)[0]["woeid"]}"
+    }
+}
+
+/* Debug info
+ output "CityInformation" {
+    value = data.null_data_source.locationwoeid.outputs
+ }
+*/
+
+
+data "http" "weather" {
+    url = data.null_data_source.locationwoeid.outputs["url"]
+    request_headers = {
+        "Accept" = "application/json"
+    }
+}
+
+###########################################################
+output "TodayForecast" {
+    value = {
+	"City" = jsondecode(data.http.weather.body)["title"]
+	"lowest" = floor(jsondecode(data.http.weather.body)["consolidated_weather"][0]["min_temp"])
+	"highest" = ceil(jsondecode(data.http.weather.body)["consolidated_weather"][0]["max_temp"])
+    }
+}
